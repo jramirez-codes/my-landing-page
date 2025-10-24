e3e014c12b0ddf95e0f3fa5120561e92af1f624f
Jordan Ramirez <jordan.s.ramirez@gmail.com>
2025-10-23 22:44:10 -0400

    new duration

--- src/components/background.tsx
+++ src/components/background.tsx
@@ -89,7 +89,7 @@ const Scene = () => {
     gsap.to(uniforms.uProgress, {
       value: 1,
       repeat: -1,
-      duration: 3,
+      duration: 5,
       ease: 'power1.out',
     });
   }, [uniforms.uProgress]);
