4478b8b0d5130e2401adb84923a988919427ad55
Jordan Ramirez <jordan.s.ramirez@gmail.com>
2025-10-23 23:25:32 -0400

    new landing page

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,78 @@
 import Background from "./components/background"
-
+import Shuffle from "./components/shuffle-text/shuffle-text"
 function App() {
 
   return (
     <>
       <Background>
-        <div className="font-bitcoin p-2 bg-transparent text-3xl font-semibold" >
-          Jordan
-          <br />
-          Ramirez
+        <div className="flex justify-between items-center w-[100vw] p-2">
+          <div className="font-bitcoin bg-transparent font-semibold" >
+            <Shuffle
+              className="text-xl"
+              text="Jordan"
+              shuffleDirection="right"
+              duration={0.35}
+              animationMode="evenodd"
+              shuffleTimes={1}
+              ease="power3.out"
+              stagger={0.03}
+              threshold={0.1}
+              triggerOnce={true}
+              triggerOnHover={true}
+              respectReducedMotion={true}
+
+            />
+            <br />
+            <Shuffle
+              className="text-xl"
+              text="Ramirez"
+              shuffleDirection="right"
+              duration={0.35}
+              animationMode="evenodd"
+              shuffleTimes={1}
+              ease="power3.out"
+              stagger={0.03}
+              threshold={0.1}
+              triggerOnce={true}
+              triggerOnHover={true}
+              respectReducedMotion={true}
+
+            />
+          </div>
+          <div className="flex justify-between items-center space-x-4">
+            <a href="https://github.com/jramirez-codes">
+              <Shuffle
+                className="text-xl hover:text-red-500 hover:font-semibold transition-colors duration-500"
+                text="GitHub"
+                shuffleDirection="right"
+                duration={0.35}
+                animationMode="evenodd"
+                shuffleTimes={1}
+                ease="power3.out"
+                stagger={0.03}
+                threshold={0.1}
+                triggerOnce={true}
+                triggerOnHover={true}
+                respectReducedMotion={true}
+              />
+            </a>
+            <a href="https://www.linkedin.com/in/jordan-ramirez/">
+              <Shuffle
+                className="text-xl hover:text-red-500 hover:font-semibold transition-colors duration-500"
+                text="LinkedIn"
+                shuffleDirection="right"
+                duration={0.35}
+                animationMode="evenodd"
+                shuffleTimes={1}
+                ease="power3.out"
+                stagger={0.03}
+                threshold={0.1}
+                triggerOnce={true}
+                triggerOnHover={true}
+                respectReducedMotion={true}
+              />
+            </a>
+          </div>
         </div>
       </Background>
     </>
